3f6fef963cebaeada1722d3c4ff8f034b81f0109
Quintin Vomocil <quintinvomocil@gmail.com>
2022-06-16 12:52:36 -0600

    Arrays included.

--- arrays.js
+++ arrays.js
@@ -2,3 +2,31 @@
     In this file, we'll be continuing our story
     with Jon being allowed to flee his fight with Jamie.
 */
+
+let backpack = [];
+
+backpack.push('Lightsaber');
+backpack.push('Robe', 'Food');
+
+//console.log(backpack);
+
+let belt = backpack.shift();
+
+backpack.splice(1, 0, 'Fur Coat');
+
+let bag = []
+
+bag.unshift('Flint', 'Blanket', 'Toothbrush');
+
+
+backpack.push(bag.slice(0, 3))
+
+//console.log(backpack)
+
+for(let i = 0; i < bag.length; i++){
+    console.log(bag[i])
+}
+
+for(let i = 0; i < 3; i++){
+    console.log(backpack[i])
+}
